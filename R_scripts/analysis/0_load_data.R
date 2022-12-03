57b89cbcd51f7ea06af58c581332da1a951fc0e3
Ariel <ariellenske@trentu.ca>
2022-12-02 16:34:15 -0800

    Initial commit

--- R_scripts/analysis/0_load_data.R
+++ R_scripts/analysis/0_load_data.R
@@ -1,7 +1,40 @@
 ###############################################################################
-#project: 
+#project: PIGU nestcam datasheet autofill
 #load data
-#date:
-#author: 
+#date: Dec 2, 2022
+#author: Ariel Lenske
 ###############################################################################
+
+##Packages
+library(googledrive)
+library(tidyverse)
+
+#download the datasheet
+drive_download("PIGU_nestbox_video_data.xlsx", overwrite = TRUE)
+
+#retrieve file names 
+#all the files in the main folder
+drive_ls("PIGU nestbox videos")
+
+#pull out the box IDs for 2022
+boxIDs <- drive_ls("PIGU2022") %>% 
+  dplyr::select(name) %>% pull()
+
+#pull out the file names for each recording event
+revents <- list()
+
+for(i in 1:length(boxIDs)){
+  
+  revents[[i]] <- drive_ls("Unit132841") %>% 
+    dplyr::select(name) %>% pull()
+  
+}
+
+
+#all the files in each recording instance folder
+
+
+
+#load datasheet into R
+
 
